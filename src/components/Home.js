40f7317d184447b7e6aad27a542b3bb922c5ffac
Nipun VV <nipunvv@gmail.com>
2022-10-19 01:36:19 +0530

    Add material design

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,47 @@
+import { Box, Card, CardContent, CardMedia, Typography } from "@mui/material";
 import { useEffect } from "react";
 import NavBar from "./NavBar";
 
 export default function Home() {
 
   return (
-    <div style={{ width: '100%', height: 100 }}>
+    <Box style={{ width: '100%', height: 100 }}>
       <NavBar />
-      <h1>Hello</h1>
-    </div>
+      <Box sx={{height: '2rem'}}/>
+      <Box sx={{ color: 'black', width: '100%' }}>
+        <Box sx={{ width: '100%' }}>
+          <Typography
+            fontSize={36}
+            fontWeight='800'
+            sx={{
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              width: 'fit-content'
+            }}>
+            Images
+          </Typography>
+        </Box>
+        <Box sx={{height: '2rem'}}/>
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="/texture.jpg"
+              alt="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over 6,000
+                species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      </Box>
+    </Box>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
 import { Link } from "react-router-dom";
+import Box from '@mui/material/Box';
 
 export default function NavBar() {
   return (
-    <div
-      style={{ backgroundColor: '#282c34', color: 'white', width: '100%', display: 'flex' }}>
-      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', padding: '1.5rem'}}>
-        <div>PUZZLE</div>
-        <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
-          <Link to='/upload' style={{fontSize: '1rem', color: 'white', textDecoration: 'none'}} >UPLOAD</Link>
-        </div>
-      </div>
-    </div>
+    <Box
+      sx={{ backgroundColor: '#282c34', color: 'white', width: '100%', display: 'flex' }}>
+      <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%', padding: '1.5rem' }}>
+        <Box>PUZZLE</Box>
+        <Box style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <Link to='/upload' style={{ fontSize: '1rem', color: 'white', textDecoration: 'none' }} >UPLOAD</Link>
+        </Box>
+      </Box>
+    </Box>
   );
 }
